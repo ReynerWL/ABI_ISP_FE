df55355731e380f961ded497727b46bc6eae51a4
moonrill <arilramadani245@gmail.com>
2025-08-06 11:47:11 +0700

    feat: add deps & icon

--- app/head.tsx
+++ app/head.tsx
@@ -1,10 +1,13 @@
 export default function Head() {
   return (
-    <>
-      <title>Nextjs 13 Boilerplate</title>
+    <head>
+      <title>Amartanet - Solusi Digital Terdepan untuk Bisnis Modern</title>
       <meta content='width=device-width, initial-scale=1' name='viewport' />
-      <meta name='description' content='Boilerplate' />
+      <meta
+        name='description'
+        content='Solusi Digital Terdepan untuk Bisnis Modern'
+      />
       <link rel='icon' href='/favicon.ico' />
-    </>
+    </head>
   )
 }
