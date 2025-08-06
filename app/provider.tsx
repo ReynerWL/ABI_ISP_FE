b7795b87a981200f0438ae499b2648299f555397
moonrill <arilramadani245@gmail.com>
2025-08-06 14:58:20 +0700

    feat: search & range picker component

--- app/provider.tsx
+++ app/provider.tsx
@@ -7,7 +7,14 @@ import { ConfigProvider } from 'antd'
 TokenUtil.loadToken()
 export const Provider = ({ children }: { children: React.ReactNode }) => {
   return (
-    <ConfigProvider theme={{ token: { colorPrimary: '#00b96b' } }}>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#00b96b',
+          fontFamily: 'var(--font-plus-jakarta-sans), sans-serif'
+        }
+      }}
+    >
       <AntdRegistry>{children}</AntdRegistry>
     </ConfigProvider>
   )
