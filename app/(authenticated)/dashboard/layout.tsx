db01ead2b54ede26cef3c0ea4f9cc503bc415fbe
moonrill <arilramadani245@gmail.com>
2025-08-07 14:42:23 +0700

    feat: dashboard layout

---
+++ app/(authenticated)/dashboard/layout.tsx
@@ -0,0 +1,91 @@
+'use client'
+
+import type { MenuProps } from 'antd'
+import { Avatar, Layout, Menu } from 'antd'
+import { Content, Header } from 'antd/es/layout/layout'
+import Sider from 'antd/es/layout/Sider'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import React from 'react'
+import {
+  HiChevronDown,
+  HiDocumentMagnifyingGlass,
+  HiInboxArrowDown,
+  HiUsers
+} from 'react-icons/hi2'
+import { TbLayoutDashboardFilled } from 'react-icons/tb'
+
+const items: MenuProps['items'] = [
+  {
+    key: 'dashboard',
+    label: <Link href={'/dashboard'}>Dashboard</Link>,
+    icon: <TbLayoutDashboardFilled className='!text-xl' />
+  },
+  {
+    key: 'data-pelanggan',
+    label: <Link href={'/dashboard/data-pelanggan'}>Data Pelanggan</Link>,
+    icon: <HiInboxArrowDown className='!text-xl' />
+  },
+  {
+    key: 'transaksi',
+    label: <Link href={'/dashboard/transaksi'}>Transaksi</Link>,
+    icon: <HiDocumentMagnifyingGlass className='!text-xl' />
+  },
+  {
+    key: 'users',
+    label: <Link href={'/dashboard/users'}>Users</Link>,
+    icon: <HiUsers className='!text-xl' />
+  }
+]
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const pathname = usePathname()
+  const selectedKeys = pathname?.split('/').filter(Boolean)
+
+  return (
+    <Layout hasSider>
+      <Sider className='dashboard-sider' width={288}>
+        <Link href={'/'}>
+          <Image
+            src={'/logo.png'}
+            alt={'logo'}
+            width={187}
+            height={56}
+            className={'cursor-pointer'}
+          />
+        </Link>
+        <Menu
+          mode='inline'
+          selectedKeys={selectedKeys}
+          items={items}
+          className='dashboard-menu !mt-6'
+        />
+      </Sider>
+      <Layout className='!bg-slate-50'>
+        <Header className='dashboard-header'>
+          <div className='flex items-center gap-4'>
+            <Avatar size={48} className='!bg-[#FFA600] !text-xl font-semibold'>
+              CS
+            </Avatar>
+            <div>
+              <h1 className='text-lg font-bold text-[#0049AC]'>
+                Cecilia Siregar
+              </h1>
+              <p className='text-sm font-medium text-slate-400'>Admin</p>
+            </div>
+            <HiChevronDown
+              className='ml-2 rounded-full border border-slate-200 p-1 text-2xl text-slate-500'
+              strokeWidth={0.8}
+            />
+          </div>
+        </Header>
+        <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+          {children}
+        </Content>
+      </Layout>
+    </Layout>
+  )
+}
+
+export default DashboardLayout
--- components/reusable/DataTable.tsx
+++ components/reusable/DataTable.tsx
@@ -31,7 +31,7 @@ const DataTable = ({
       footer={() => (
         <div className='flex w-full justify-between'>
           <div className='flex items-center gap-2 font-semibold text-slate-400'>
-            <p>Tampilkan</p>
+            <p className='hidden md:block'>Tampilkan</p>
             <Select
               options={[
                 { value: 10, label: '10 / halaman' },
@@ -44,28 +44,32 @@ const DataTable = ({
                 <HiOutlineChevronDown className='text-lg' strokeWidth={2.5} />
               }
             />
-            <p>dari {totalData} data</p>
+            <p className='hidden md:block'>dari {totalData} data</p>
           </div>
-          <div className='flex items-center gap-6'>
+          <div className='flex items-center gap-2 md:gap-4'>
             <Button
               color='default'
               variant='filled'
-              className='!h-fit !w-fit !rounded-full border-none !bg-slate-100 !px-4 !py-[7px] !font-semibold !text-slate-400 hover:!bg-slate-200 disabled:!border-0 disabled:!bg-slate-50 disabled:!text-slate-300'
+              className='!h-fit !w-fit !rounded-full border-none !bg-slate-100 !py-[7px] !font-semibold !text-slate-400 hover:!bg-slate-200 disabled:!border-0 disabled:!bg-slate-50 disabled:!text-slate-300 sm:!px-4'
               iconPosition='start'
-              icon={<HiOutlineChevronLeft strokeWidth={3} />}
+              icon={
+                <HiOutlineChevronLeft strokeWidth={3} className='text-base' />
+              }
               disabled={page === 1}
             >
-              Kembali
+              <span className='hidden sm:block'>Kembali</span>
             </Button>
             <Button
               color='orange'
               variant='filled'
-              className='!h-fit !w-fit !rounded-full !border-none !bg-[#FFF7ED] !px-4 !py-[7px] !font-semibold !text-[#F59E0B] hover:!bg-orange-100 disabled:!bg-orange-50 disabled:!text-orange-300'
+              className='!h-fit !w-fit !rounded-full !border-none !bg-[#FFF7ED] !py-[7px] !font-semibold !text-[#F59E0B] hover:!bg-orange-100 disabled:!bg-orange-50 disabled:!text-orange-300 sm:!px-4'
               iconPosition='end'
-              icon={<HiOutlineChevronRight strokeWidth={3} />}
+              icon={
+                <HiOutlineChevronRight strokeWidth={3} className='text-base' />
+              }
               disabled={page === totalPage}
             >
-              Lanjut
+              <span className='hidden sm:block'>Lanjut</span>
             </Button>
           </div>
         </div>
