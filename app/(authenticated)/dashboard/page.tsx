ea0ba807e94a7056550617167ffb519a95557be2
moonrill <arilramadani245@gmail.com>
2025-08-14 16:56:22 +0700

    fix: scrollable dashboard layout

--- app/(authenticated)/dashboard/page.tsx
+++ app/(authenticated)/dashboard/page.tsx
@@ -2,8 +2,13 @@ import Title from '#/components/reusable/Title'
 
 const Dashboard = () => {
   return (
-    <div>
+    <div className='h-full w-full'>
       <Title>Dashboard</Title>
+      <div className='mt-6 grid h-screen w-full grid-rows-3 gap-6'>
+        <div className='bg-red-200'></div>
+        <div className='bg-red-200'></div>
+        <div className='bg-red-200'></div>
+      </div>
     </div>
   )
 }
