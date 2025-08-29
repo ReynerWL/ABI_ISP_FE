308e096b308d4ba1de4a2ac6f269743a6cc0967e
moonrill <arilramadani245@gmail.com>
2025-08-29 14:45:35 +0700

    feat: slicing data pelanggan

--- components/reusable/CustomDateRangePicker.tsx
+++ components/reusable/CustomDateRangePicker.tsx
@@ -1,14 +1,40 @@
+'use client'
+
 import { DatePicker } from 'antd'
+import dayjs from 'dayjs'
+import { useRouter, useSearchParams } from 'next/navigation'
 import { HiOutlineCalendar, HiXMark } from 'react-icons/hi2'
 
+const { RangePicker } = DatePicker
+
 const CustomDateRangePicker = () => {
+  const router = useRouter()
+  const searchParams = useSearchParams()
+  const dateRange = searchParams?.get('dateRange') || ''
+
+  const handleChange = (dates: any) => {
+    const queryParams = new URLSearchParams(searchParams?.toString())
+
+    if (dates) {
+      const startDate = dayjs(dates[0]).format('YYYY-MM-DD')
+      const endDate = dayjs(dates[1]).format('YYYY-MM-DD')
+      queryParams.set('dateRange', `${startDate},${endDate}`)
+    } else {
+      queryParams.delete('dateRange')
+    }
+
+    router.push(`?${queryParams.toString()}`)
+  }
+
   return (
-    <DatePicker.RangePicker
+    <RangePicker
       className='custom-date-range-picker'
       placeholder={['Tanggal Awal', 'Tanggal Akhir']}
       suffixIcon={
         <HiOutlineCalendar
-          className='text-xl text-slate-400'
+          className={`text-xl transition duration-200 ${
+            dateRange ? 'text-secondary' : 'text-slate-400'
+          }`}
           strokeWidth={1.8}
         />
       }
@@ -16,11 +42,12 @@ const CustomDateRangePicker = () => {
       allowClear={{
         clearIcon: (
           <HiXMark
-            className='text-xl text-slate-400 transition duration-200 hover:text-slate-600'
+            className={`text-xl text-slate-400 transition duration-200 hover:text-slate-600`}
             strokeWidth={0.4}
           />
         )
       }}
+      onChange={handleChange}
     />
   )
 }
