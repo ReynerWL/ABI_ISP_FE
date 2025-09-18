16ee1b9b8fed85c962f30604a879b668da125d14
moonrill <arilramadani245@gmail.com>
2025-09-18 15:29:18 +0700

    feat: detail data pelanggan

--- app/(authenticated)/dashboard/data-pelanggan/[id]/page.tsx
+++ app/(authenticated)/dashboard/data-pelanggan/[id]/page.tsx
@@ -1,92 +1,69 @@
-import Chip from '#/components/reusable/Chip'
+'use client'
+
+import InfoPaketCard from '#/components/data-pelanggan/InfoPaketCard'
+import InfoPelanggan from '#/components/data-pelanggan/InfoPelanggan'
+import InstalasiPreview from '#/components/data-pelanggan/InstalasiPreview'
+import KTPPreview from '#/components/data-pelanggan/KTPPreview'
 import Title from '#/components/reusable/Title'
-import { Image } from 'antd'
+import { Button } from 'antd'
 
 const DetailPelanggan = () => {
   return (
     <div className='space-y-8'>
       <Title>Detail Pelanggan</Title>
-      <div className='flex w-full gap-6'>
-        <div className='flex w-full max-w-[940px] flex-col gap-6'>
-          <div className='flex w-full flex-col gap-8 rounded-2xl bg-white p-6'>
-            <div className='flex justify-between'>
-              <div className='flex items-center gap-2'>
-                <div className='h-full w-2 rounded-sm bg-secondary'></div>
-                <div className='flex flex-col gap-1 font-semibold'>
-                  <h1 className='text-xl text-slate-700'>
-                    Informasi Pelanggan
-                  </h1>
-                  <h2 className='text-base text-slate-500'>
-                    ID Pelanggan :{' '}
-                    <span className='text-primary'>121846234310</span>
-                  </h2>
-                </div>
-              </div>
-              <div className='flex items-center'>
-                <Chip text='Pending' color='orange' />
-              </div>
-            </div>
-            <div className='grid grid-cols-2 gap-y-6'>
-              <div className='flex flex-col gap-1'>
-                <p className='text-xs font-medium leading-[14px] text-slate-500'>
-                  Tanggal Berlangganan
-                </p>
-                <h3 className='font-semibold text-slate-700'>
-                  01 Januari 2025
-                </h3>
-              </div>
-              <div className='flex flex-col gap-1'>
-                <p className='text-xs font-medium leading-[14px] text-slate-500'>
-                  Nama Pelanggan
-                </p>
-                <h3 className='font-semibold text-slate-700'>Edward</h3>
-              </div>
-              <div className='flex flex-col gap-1'>
-                <p className='text-xs font-medium leading-[14px] text-slate-500'>
-                  Email
-                </p>
-                <h3 className='font-semibold text-slate-700'>
-                  edward205@gmail.com
-                </h3>
-              </div>
-              <div className='flex flex-col gap-1'>
-                <p className='text-xs font-medium leading-[14px] text-slate-500'>
-                  No. Telp
-                </p>
-                <h3 className='font-semibold text-slate-700'>085178527835</h3>
-              </div>
-              <div className='flex flex-col gap-1'>
-                <p className='text-xs font-medium leading-[14px] text-slate-500'>
-                  Alamat
-                </p>
-                <h3 className='font-semibold text-slate-700'>
-                  Jl.Bintara 14 No.08, RT.001/RW.009
-                </h3>
-              </div>
-            </div>
-          </div>
-          <div className='grid w-full grid-cols-2 gap-6 rounded-2xl bg-white p-6'>
-            <div className='flex flex-col gap-6'>
+      <div className='flex w-full flex-col gap-6 md:flex-row'>
+        <div className='flex w-full flex-col gap-6 2xl:w-full'>
+          <InfoPelanggan
+            id='1234'
+            tanggalBerlangganan='2023-01-01'
+            status='Pending'
+            namaPelanggan='John Doe'
+            email='john.doe@me.com'
+            noTelp='08123456789'
+            alamat='Jl. Raya, Jakarta'
+          />
+          <div className='grid w-full gap-6 rounded-2xl bg-white p-6 xl:grid-cols-2'>
+            <div className='flex flex-col gap-4'>
               <h1 className='text-xl font-semibold text-slate-700'>Foto KTP</h1>
-              <Image
-                src='/ktp.png'
-                alt={'ktp'}
-                className='h-auto max-h-[250px] w-full max-w-full rounded-xl object-cover'
-              />
+              <KTPPreview imageUrl='/ktp.png' />
             </div>
-            <div className='flex flex-col gap-6'>
+            <div className='flex flex-col gap-4'>
               <div className='flex items-center justify-between font-semibold'>
                 <h1 className='text-xl text-slate-700'>Foto Instalasi</h1>
                 <h2 className='text-slate-500'>01-08-2025</h2>
               </div>
-              <Image
-                src='/router.jpg'
-                alt={'ktp'}
-                className='h-auto max-h-[250px] w-full max-w-full rounded-xl object-cover'
-              />
+              <InstalasiPreview />
             </div>
           </div>
         </div>
+        <InfoPaketCard />
+      </div>
+      <div className='flex w-full justify-end gap-4'>
+        <Button
+          type='primary'
+          className={`!h-full w-fit !rounded-lg !bg-white !px-5 !py-2 !font-semibold !text-slate-500 !shadow-none hover:!bg-opacity-70`}
+        >
+          Kembali
+        </Button>
+        {}
+        <Button
+          type='primary'
+          className={`!h-full w-fit !rounded-lg !bg-red-50 !px-5 !py-2 !font-semibold !text-red-500 !shadow-none hover:!bg-red-100`}
+        >
+          Menolak
+        </Button>
+        <Button
+          type='primary'
+          className={`!h-full w-fit !rounded-lg !px-5 !py-2 !font-semibold !shadow-none`}
+        >
+          Setujui
+        </Button>
+        <Button
+          type='primary'
+          className={`!h-full w-fit !rounded-lg !px-5 !py-2 !font-semibold !shadow-none`}
+        >
+          Aktifkan
+        </Button>
       </div>
     </div>
   )
---
+++ components/data-pelanggan/InfoPelanggan.tsx
@@ -0,0 +1,81 @@
+import Chip from '../reusable/Chip'
+
+interface InfoPelangganProps {
+  id: string
+  tanggalBerlangganan: string
+  namaPelanggan: string
+  email: string
+  noTelp: string
+  alamat: string
+  status: 'Baru' | 'Aktif' | 'Pending' | 'Nonaktif'
+}
+
+const InfoPelanggan = ({
+  id,
+  tanggalBerlangganan,
+  namaPelanggan,
+  email,
+  noTelp,
+  alamat,
+  status
+}: InfoPelangganProps) => {
+  const chipColor: Record<
+    InfoPelangganProps['status'],
+    'green' | 'orange' | 'red' | 'gray'
+  > = { Baru: 'gray', Aktif: 'green', Pending: 'orange', Nonaktif: 'red' }
+
+  return (
+    <div className='flex w-full flex-col gap-8 rounded-2xl bg-white p-6'>
+      <div className='flex justify-between'>
+        <div className='flex items-center gap-2'>
+          <div className='h-full w-2 rounded-sm bg-secondary'></div>
+          <div className='flex flex-col gap-1 font-semibold'>
+            <h1 className='text-xl text-slate-700'>Informasi Pelanggan</h1>
+            <h2 className='text-base text-slate-500'>
+              ID Pelanggan : <span className='text-primary'>{id}</span>
+            </h2>
+          </div>
+        </div>
+        <div className='flex items-center'>
+          <Chip text='Pending' color={chipColor[status]} />
+        </div>
+      </div>
+      <div className='grid grid-cols-2 gap-y-6'>
+        <div className='flex flex-col gap-1'>
+          <p className='text-xs font-medium leading-[14px] text-slate-500'>
+            Tanggal Berlangganan
+          </p>
+          <h3 className='font-semibold text-slate-700'>
+            {tanggalBerlangganan}
+          </h3>
+        </div>
+        <div className='flex flex-col gap-1'>
+          <p className='text-xs font-medium leading-[14px] text-slate-500'>
+            Nama Pelanggan
+          </p>
+          <h3 className='font-semibold text-slate-700'>{namaPelanggan}</h3>
+        </div>
+        <div className='flex flex-col gap-1'>
+          <p className='text-xs font-medium leading-[14px] text-slate-500'>
+            Email
+          </p>
+          <h3 className='font-semibold text-slate-700'>{email}</h3>
+        </div>
+        <div className='flex flex-col gap-1'>
+          <p className='text-xs font-medium leading-[14px] text-slate-500'>
+            No. Telp
+          </p>
+          <h3 className='font-semibold text-slate-700'>{noTelp}</h3>
+        </div>
+        <div className='flex flex-col gap-1'>
+          <p className='text-xs font-medium leading-[14px] text-slate-500'>
+            Alamat
+          </p>
+          <h3 className='font-semibold text-slate-700'>{alamat}</h3>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default InfoPelanggan
