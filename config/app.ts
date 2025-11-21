c1144dc09f411d673a4d3fff4192e5957319167b
ReynerWL <reynerwl2018@gmail.com>
2025-11-21 10:25:20 +0700

    Fix: app.ts

--- config/app.ts
+++ config/app.ts
@@ -4,5 +4,5 @@ export const config = {
       ? (window as any).serverEnv?.DYNAMIC_ENV_BASE_URL
       : '') ||
     process.env.NEXT_PUBLIC_BASE_URL ||
-    'https://abi-isp-be.k3s.bangun-kreatif.com'
+    'https://api.mbinet.click'
 }
