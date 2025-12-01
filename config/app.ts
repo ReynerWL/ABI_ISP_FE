7729f2707d5b7921b2495fe46707604f645456ea
moonrill <arilramadani245@gmail.com>
2025-12-01 12:37:03 +0700

    fix: app config

--- config/app.ts
+++ config/app.ts
@@ -4,7 +4,6 @@ export const config = {
       ? (window as any).serverEnv?.DYNAMIC_ENV_BASE_URL
       : '') ||
     process.env.NEXT_PUBLIC_BASE_URL ||
-    'http://172.17.0.186:3222' ||
     'https://api.mbinet.click',
   fileUrl: 'https://file.bangun-kreatif.com:443/abiisp'
 }
